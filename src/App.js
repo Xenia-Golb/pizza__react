af32db40da0b860027f523d494b9aead7198c9df
Xenia Golbraykh <karzerin@gmail.com>
2024-11-22 20:13:26 +0300

    L6  add fetch, useeffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import './sass/app.scss';
-import Categories from './components/categories/Categories';
-import Header from './components/header/Header';
-import Sort from './components/sort/Sort';
-import PizzaBlock from './components/pizza-block/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import "./sass/app.scss";
+import Categories from "./components/categories/Categories";
+import Header from "./components/header/Header";
+import Sort from "./components/sort/Sort";
+import PizzaBlock from "./components/pizza-block/PizzaBlock";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://6740b1c4d0b59228b7f10754.mockapi.io/items")
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-          <Categories />
-          <Sort />
+            <Categories />
+            <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-           {pizzas.map((obj)=>{
-             return <PizzaBlock key={obj.id} 
-             {...obj} />
-           })}
+            {items.map((obj) => {
+              return <PizzaBlock key={obj.id} {...obj} />;
+            })}
           </div>
         </div>
       </div>
